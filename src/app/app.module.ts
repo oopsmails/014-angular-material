d838fce40a8d649adbc4f52933a7ec9545129808
oopsmails <oopsmails@gmail.com>
2022-12-19 16:02:28 -0500

    20221219: added home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, AppRoutingModule],
+  imports: [BrowserModule, SharedModule, HomeModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
